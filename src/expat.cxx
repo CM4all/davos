ae1f2cbf628d07e59f67a98ce22bfd3efff1aec2
Max Kellermann <mk@cm4all.com>
2015-05-11 20:24:26 +0200

    build with libwas 0.8

--- src/expat.cxx
+++ src/expat.cxx
@@ -23,52 +23,23 @@ ExpatParser::Parse(was_simple *w)
 
     while (true) {
         char buffer[4096];
-
-        enum was_simple_poll_result result = was_simple_input_poll(w, -1);
-        switch (result) {
-            ssize_t nbytes;
-
-        case WAS_SIMPLE_POLL_SUCCESS:
-            nbytes = was_simple_input_read(w, buffer, sizeof(buffer));
-            if (nbytes < 0) {
-                if (errno == EAGAIN)
-                    continue;
-
-                fprintf(stderr, "Error reading HTTP request body: %s\n",
-                        strerror(errno));
-                return false;
-            }
-
-            if (nbytes == 0) {
-                fprintf(stderr, "Unexpected end of HTTP request body\n");
-                return false;
-            }
-
-            if (XML_Parse(parser, buffer, nbytes, false) != XML_STATUS_OK) {
-                fprintf(stderr, "XML parser failed on HTTP request body\n");
-                return false;
-            }
-
-            break;
-
-        case WAS_SIMPLE_POLL_ERROR:
-            fprintf(stderr, "Error reading HTTP request body.\n");
+        ssize_t nbytes = was_simple_read(w, buffer, sizeof(buffer));
+        if (nbytes < 0) {
+            fprintf(stderr, "Error reading HTTP request body\n");
             return false;
+        }
 
-        case WAS_SIMPLE_POLL_TIMEOUT:
-            fprintf(stderr, "Timeout reading HTTP request body.\n");
-            return false;
-
-        case WAS_SIMPLE_POLL_END:
+        if (nbytes == 0) {
             if (XML_Parse(parser, "", 0, true) != XML_STATUS_OK) {
                 fprintf(stderr, "XML parser failed on HTTP request body\n");
                 return false;
             }
 
             return true;
+        }
 
-        case WAS_SIMPLE_POLL_CLOSED:
-            fprintf(stderr, "Client has closed the HTTP request body.\n");
+        if (XML_Parse(parser, buffer, nbytes, false) != XML_STATUS_OK) {
+            fprintf(stderr, "XML parser failed on HTTP request body\n");
             return false;
         }
     }
