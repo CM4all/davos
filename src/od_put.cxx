b41565105e034605886072573dfb1dbd05598e4d
Max Kellermann <mk@cm4all.com>
2015-11-03 11:50:12 +0100

    od: support PUT without splice()

--- src/od_put.cxx
+++ src/od_put.cxx
@@ -14,6 +14,7 @@ extern "C" {
 #include <od/resource.h>
 #include <od/create.h>
 #include <od/stat.h>
+#include <od/error.h>
 }
 
 #include <assert.h>
@@ -37,12 +38,55 @@ SendStat(od_resource_create *c, const char *name, GError **error_r)
 }
 
 static bool
+PutDataSlow(struct od_resource_create *c, struct was_simple *w)
+{
+    while (true) {
+        auto p = was_simple_input_poll(w, -1);
+        switch (p) {
+        case WAS_SIMPLE_POLL_SUCCESS:
+            break;
+
+        case WAS_SIMPLE_POLL_END:
+            return true;
+
+        case WAS_SIMPLE_POLL_ERROR:
+        case WAS_SIMPLE_POLL_TIMEOUT:
+        case WAS_SIMPLE_POLL_CLOSED:
+            return false;
+        }
+
+        char buffer[8192];
+        ssize_t nbytes = was_simple_read(w, buffer, sizeof(buffer));
+        if (gcc_unlikely(nbytes <= 0)) {
+            if (nbytes == -1)
+                fprintf(stderr, "Failed to read from WAS pipe: %s\n",
+                        strerror(errno));
+
+            return nbytes == 0;
+        }
+
+        GError *error = nullptr;
+        if (!od_resource_create_write(c, buffer, nbytes, &error)) {
+            fprintf(stderr, "%s\n", error->message);
+            g_error_free(error);
+            return false;
+        }
+    }
+}
+
+static bool
 PutData(struct od_resource_create *c, struct was_simple *w)
 {
-    // TODO: support slow mode
     GError *error = nullptr;
     int fd = od_resource_create_open(c, &error);
     if (fd < 0) {
+        if (error->domain == od_error_domain() &&
+            error->code == OD_ERROR_NOT_IMPLEMENTED)
+            /* libod data module doesn't support file descriptor -
+               fall back to "slow" transfer using was_simple_read()
+               and od_resource_create_write() */
+            return PutDataSlow(c, w);
+
         fprintf(stderr, "%s\n", error->message);
         g_error_free(error);
         return false;
