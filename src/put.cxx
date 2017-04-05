83aab70395c81400499c5d821030afefebc51e0e
Max Kellermann <mk@cm4all.com>
2017-04-05 15:06:17 +0200

    put: use O_TMPFILE

--- src/put.cxx
+++ src/put.cxx
@@ -8,61 +8,31 @@
 #include "error.hxx"
 #include "file.hxx"
 #include "splice.hxx"
+#include "io/FileWriter.hxx"
+#include "util/PrintException.hxx"
 
 extern "C" {
 #include <was/simple.h>
 }
 
-#include <string>
-
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-
-static std::string
-make_tmp_path(const char *path)
-{
-    // TODO
-    std::string s(path);
-    s.append(".tmp");
-    return s;
-}
+#include <exception>
 
 void
 handle_put(was_simple *w, const FileResource &resource)
 {
     assert(was_simple_has_body(w));
 
-    const std::string tmp_path = make_tmp_path(resource.GetPath());
+    try {
+        FileWriter fw(resource.GetPath());
 
-    int file_fd = open(tmp_path.c_str(), O_CREAT|O_EXCL|O_WRONLY, 0666);
-    if (file_fd < 0) {
-        fprintf(stderr, "Failed to create %s: %s\n",
-                tmp_path.c_str(), strerror(errno));
-        was_simple_status(w, HTTP_STATUS_INTERNAL_SERVER_ERROR);
-        return;
-    }
+        if (!splice_from_was(w, fw.GetFileDescriptor().Get())) {
+            was_simple_status(w, HTTP_STATUS_INTERNAL_SERVER_ERROR);
+            return;
+        }
 
-    if (!splice_from_was(w, file_fd)) {
-        close(file_fd);
-        unlink(tmp_path.c_str());
-        was_simple_status(w, HTTP_STATUS_INTERNAL_SERVER_ERROR);
-        return;
-    }
-
-    if (close(file_fd) < 0) {
-        fprintf(stderr, "Failed to write %s: %s\n",
-                tmp_path.c_str(), strerror(errno));
-        unlink(tmp_path.c_str());
-        was_simple_status(w, HTTP_STATUS_INTERNAL_SERVER_ERROR);
-        return;
-    }
-
-    if (rename(tmp_path.c_str(), resource.GetPath()) < 0) {
-        fprintf(stderr, "Failed to commit %s: %s\n",
-                resource.GetPath(), strerror(errno));
-        unlink(tmp_path.c_str());
+        fw.Commit();
+    } catch (const std::exception &e) {
+        PrintException(e);
         was_simple_status(w, HTTP_STATUS_INTERNAL_SERVER_ERROR);
         return;
     }
