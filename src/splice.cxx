3a9e114d9e3dc561c0baf4ae0b58ce9ebae907ae
Max Kellermann <mk@cm4all.com>
2018-02-01 22:15:47 +0100

    splice: move WAS_SIMPLE_POLL_SUCCESS code out of the switch block

--- src/splice.cxx
+++ src/splice.cxx
@@ -27,26 +27,7 @@ splice_from_was(was_simple *w, int out_fd)
     while (true) {
         enum was_simple_poll_result result = was_simple_input_poll(w, -1);
         switch (result) {
-            ssize_t nbytes;
-
         case WAS_SIMPLE_POLL_SUCCESS:
-            nbytes = splice(in_fd, nullptr, out_fd, nullptr,
-                            1 << 30,
-                            SPLICE_F_MOVE|SPLICE_F_NONBLOCK|SPLICE_F_MORE);
-            if (nbytes < 0) {
-                if (errno == EAGAIN)
-                    continue;
-
-                fprintf(stderr, "Error copying HTTP request body: %s\n",
-                        strerror(errno));
-                return false;
-            }
-
-            if (!was_simple_received(w, nbytes)) {
-                fprintf(stderr, "Error receiving HTTP request body\n");
-                return false;
-            }
-
             break;
 
         case WAS_SIMPLE_POLL_ERROR:
@@ -62,6 +43,23 @@ splice_from_was(was_simple *w, int out_fd)
 
         case WAS_SIMPLE_POLL_CLOSED:
             fprintf(stderr, "Client has closed the PUT request body.\n");
+            return false;
+        }
+
+        auto nbytes = splice(in_fd, nullptr, out_fd, nullptr,
+                             1 << 30,
+                             SPLICE_F_MOVE|SPLICE_F_NONBLOCK|SPLICE_F_MORE);
+        if (nbytes < 0) {
+            if (errno == EAGAIN)
+                continue;
+
+            fprintf(stderr, "Error copying HTTP request body: %s\n",
+                    strerror(errno));
+            return false;
+        }
+
+        if (!was_simple_received(w, nbytes)) {
+            fprintf(stderr, "Error receiving HTTP request body\n");
             return false;
         }
     }
