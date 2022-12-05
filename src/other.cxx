845777b07913099882223a379000509f26748f7b
Max Kellermann <mk@cm4all.com>
2022-12-05 15:20:01 +0100

    other: use libcommon RecursiveCopy() instead of libfox fox_copy()

--- src/other.cxx
+++ src/other.cxx
@@ -10,11 +10,8 @@
 #include "util.hxx"
 #include "system/Error.hxx"
 #include "io/FileDescriptor.hxx"
+#include "io/RecursiveCopy.hxx"
 #include "io/RecursiveDelete.hxx"
-
-extern "C" {
-#include <fox/cp.h>
-}
 
 #include <fcntl.h>
 
@@ -37,17 +34,20 @@ handle_copy(was_simple *w, const FileResource &src, const FileResource &dest)
 	// TODO: support "Depth: 0"
 	// TODO: overwriting an existing directory?
 
-	unsigned options = FOX_CP_DEVICE|FOX_CP_INODE|FOX_CP_PRESERVE_XATTR;
+	unsigned options = RECURSIVE_COPY_ONE_FILESYSTEM;
 
 	if (!get_overwrite_header(w))
-		options |= FOX_CP_EXCL;
+		options |= RECURSIVE_COPY_NO_OVERWRITE;
 
-	fox_error_t error;
-	fox_status_t status = fox_copy(src.GetPath(), dest.GetPath(), options,
-				       &error);
-	if (status != FOX_STATUS_SUCCESS) {
-		errno_response(w, status);
-		return;
+	try {
+		RecursiveCopy(FileDescriptor{AT_FDCWD}, src.GetPath(),
+			      FileDescriptor{AT_FDCWD}, dest.GetPath(),
+			      options);
+	} catch (const std::system_error &e) {
+		if (e.code().category() == ErrnoCategory())
+			errno_response(w, e.code().value());
+		else
+			throw;
 	}
 }
 
