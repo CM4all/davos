CM4all/davos

commit c5d54116444a88478cdaf96a1a00bccf9d88a061

Writer: drop, use WasOutputStream and BufferedOutputStream instead

src/wxml.hxx
@@ -4,129 +4,134 @@
  * author: Max Kellermann <mk@cm4all.com>
  */
 
-#include "writer.hxx"
+#include "io/BufferedOutputStream.hxx"
 
-#include <algorithm>
-
-static bool
-wxml_declaration(Writer &w)
+static void
+wxml_declaration(BufferedOutputStream &o)
 {
-	return w.Write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+	o.Write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
 }
 
-static bool
-wxml_begin_tag(Writer &w, const char *name)
+static void
+wxml_begin_tag(BufferedOutputStream &o, std::string_view name)
 {
-	return w.Write("<") && w.Write(name);
+	o.Write('<');
+	o.Write(name);
 }
 
-static bool
-wxml_end_tag(Writer &w)
+static void
+wxml_end_tag(BufferedOutputStream &o)
 {
-	return w.Write(">");
+	o.Write('>');
 }
 
-static bool
-wxml_end_short_tag(Writer &w)
+static void
+wxml_end_short_tag(BufferedOutputStream &o)
 {
-	return w.Write("/>");
+	o.Write("/>");
 }
 
-static bool
-wxml_open_element(Writer &w, const char *name)
+static void
+wxml_open_element(BufferedOutputStream &o, std::string_view name)
 {
-	return wxml_begin_tag(w, name) && wxml_end_tag(w);
+	wxml_begin_tag(o, name);
+	wxml_end_tag(o);
 }
 
-static bool
-wxml_close_element(Writer &w, const char *name)
+static void
+wxml_close_element(BufferedOutputStream &o, std::string_view name)
 {
-	return w.Write("</") && w.Write(name) &&
-		wxml_end_tag(w);
+	o.Write("</");
+	o.Write(name);
+	wxml_end_tag(o);
 }
 
-inline bool
-wxml_short_element(Writer &w, const char *name)
+inline void
+wxml_short_element(BufferedOutputStream &o, std::string_view name)
 {
-	return wxml_begin_tag(w, name) && wxml_end_short_tag(w);
+	wxml_begin_tag(o, name);
+	wxml_end_short_tag(o);
 }
 
 [[gnu::nonnull]]
-bool
-wxml_cdata(Writer &w, const char *data);
+void
+wxml_cdata(BufferedOutputStream &o, const char *data);
 
-inline bool
-wxml_string_element(Writer &w, const char *name, const char *value)
+inline void
+wxml_string_element(BufferedOutputStream &o, std::string_view name,
+		    const char *value)
 {
-	return wxml_open_element(w, name) &&
-		wxml_cdata(w, value) &&
-		wxml_close_element(w, name);
+	wxml_open_element(o, name);
+	wxml_cdata(o, value);
+	wxml_close_element(o, name);
 }
 
-template<typename... Args>
-static bool
-wxml_format_element(Writer &w, const char *name, const char *fmt,
-		    Args... args)
+template<typename S, typename... Args>
+static void
+wxml_fmt_element(BufferedOutputStream &o, std::string_view name,
+		 const S &fmt, Args&&... args)
 {
-	return wxml_open_element(w, name) &&
-		w.Format(fmt, std::forward<Args>(args)...) &&
-		wxml_close_element(w, name);
+	wxml_open_element(o, name);
+	o.Fmt(fmt, fmt, std::forward<Args>(args)...);
+	wxml_close_element(o, name);
 }
 
-static bool
-wxml_attribute(Writer &w, const char *name, const char *value)
+static void
+wxml_attribute(BufferedOutputStream &o, std::string_view name, const char *value)
 {
-	return w.Write(" ") && w.Write(name) &&
-		w.Write("=\"") && wxml_cdata(w, value) &&
-		w.Write("\"");
+	o.Write(' ');
+	o.Write(name);
+	o.Write("=\"");
+	wxml_cdata(o, value);
+	o.Write("\"");
 }
 
-inline bool
-begin_multistatus(Writer &w)
+inline void
+begin_multistatus(BufferedOutputStream &o)
 {
-	return wxml_declaration(w) &&
-		wxml_begin_tag(w, "D:multistatus") &&
-		wxml_attribute(w, "xmlns:D", "DAV:") &&
-		wxml_end_tag(w);
+	wxml_declaration(o);
+	wxml_begin_tag(o, "D:multistatus");
+	wxml_attribute(o, "xmlns:D", "DAV:");
+	wxml_end_tag(o);
 }
 
-inline bool
-end_multistatus(Writer &w)
+inline void
+end_multistatus(BufferedOutputStream &o)
 {
-	return wxml_close_element(w, "D:multistatus");
+	wxml_close_element(o, "D:multistatus");
 }
 
 /**
  * @param uri an escaped URI
  */
-inline bool
-href(Writer &w, const char *uri)
+inline void
+href(BufferedOutputStream &o, const char *uri)
 {
-	return wxml_string_element(w, "D:href", uri);
+	wxml_string_element(o, "D:href", uri);
 }
 
-inline bool
-resourcetype_collection(Writer &w)
+inline void
+resourcetype_collection(BufferedOutputStream &o)
 {
-	return wxml_open_element(w, "D:resourcetype") &&
-		wxml_short_element(w, "D:collection") &&
-		wxml_close_element(w, "D:resourcetype");
+	wxml_open_element(o, "D:resourcetype");
+	wxml_short_element(o, "D:collection");
+	wxml_close_element(o, "D:resourcetype");
 }
 
-inline bool
-open_response_prop(Writer &w, const char *uri, const char *status)
+inline void
+open_response_prop(BufferedOutputStream &o, const char *uri, const char *status)
 {
-	return wxml_open_element(w, "D:response") &&
-		href(w, uri) &&
-		wxml_open_element(w, "D:propstat") &&
-		wxml_string_element(w, "D:status", status) &&
-		wxml_open_element(w, "D:prop");
+	wxml_open_element(o, "D:response");
+	href(o, uri);
+	wxml_open_element(o, "D:propstat");
+	wxml_string_element(o, "D:status", status);
+	wxml_open_element(o, "D:prop");
 }
 
-inline bool
-close_response_prop(Writer &w)
+inline void
+close_response_prop(BufferedOutputStream &o)
 {
-	return wxml_close_element(w, "D:prop") &&
-		wxml_close_element(w, "D:propstat") &&
-		wxml_close_element(w, "D:response");
+	wxml_close_element(o, "D:prop");
+	wxml_close_element(o, "D:propstat");
+	wxml_close_element(o, "D:response");
 }

src/frontend.hxx
@@ -6,6 +6,7 @@
 
 #include "was.hxx"
 #include "was/Loop.hxx"
+#include "was/WasOutputStream.hxx"
 #include "util/UriEscape.hxx"
 #include "util/LightString.hxx"
 #include "util/ScopeExit.hxx"
@@ -326,6 +327,7 @@ try {
 	default:
 		was_simple_status(was, HTTP_STATUS_METHOD_NOT_ALLOWED);
 	}
+} catch (WasOutputStream::WriteFailed) {
 } catch (MalformedUri) {
 	was_simple_status(was, HTTP_STATUS_BAD_REQUEST);
 } catch (OutsideUri) {

src/lock.cxx
@@ -8,7 +8,7 @@
 #include "wxml.hxx"
 #include "error.hxx"
 #include "expat.hxx"
-#include "util/Compiler.h"
+#include "was/WasOutputStream.hxx"
 
 #include <was/simple.h>
 
@@ -17,35 +17,35 @@
 
 #include <string.h>
 
-static bool
-begin_prop(Writer &w)
+static void
+begin_prop(BufferedOutputStream &o)
 {
-	return wxml_declaration(w) &&
-		wxml_begin_tag(w, "D:prop") &&
-		wxml_attribute(w, "xmlns:D", "DAV:") &&
-		wxml_end_tag(w);
+	wxml_declaration(o);
+	wxml_begin_tag(o, "D:prop");
+	wxml_attribute(o, "xmlns:D", "DAV:");
+	wxml_end_tag(o);
 }
 
-static bool
-end_prop(Writer &w)
+static void
+end_prop(BufferedOutputStream &o)
 {
-	return wxml_close_element(w, "D:prop");
+	wxml_close_element(o, "D:prop");
 }
 
-static bool
-locktoken_href(Writer &w, const char *token)
+static void
+locktoken_href(BufferedOutputStream &o, const char *token)
 {
-	return wxml_open_element(w, "D:locktoken") &&
-		href(w, token) &&
-		wxml_close_element(w, "D:locktoken");
+	wxml_open_element(o, "D:locktoken");
+	href(o, token);
+	wxml_close_element(o, "D:locktoken");
 }
 
-static bool
-owner_href(Writer &w, const char *token)
+static void
+owner_href(BufferedOutputStream &o, const char *token)
 {
-	return wxml_open_element(w, "D:owner") &&
-		href(w, token) &&
-		wxml_close_element(w, "D:owner");
+	wxml_open_element(o, "D:owner");
+	href(o, token);
+	wxml_close_element(o, "D:owner");
 }
 
 static void XMLCALL
@@ -139,25 +139,29 @@ LockMethod::Run(was_simple *w, bool created)
 	    !was_simple_set_header(w, "lock-token", token2))
 		return;
 
-	Writer writer(w);
-	begin_prop(writer) &&
-		wxml_open_element(writer, "D:lockdiscovery") &&
-		wxml_open_element(writer, "D:activelock") &&
+	WasOutputStream wos{w};
+	BufferedOutputStream bos{wos};
 
-		wxml_open_element(writer, "D:locktype") &&
-		wxml_short_element(writer, "D:write") &&
-		wxml_close_element(writer, "D:locktype") &&
+	begin_prop(bos);
+	wxml_open_element(bos, "D:lockdiscovery");
+	wxml_open_element(bos, "D:activelock");
 
-		wxml_open_element(writer, "D:lockscope") &&
-		wxml_short_element(writer, "D:exclusive") &&
-		wxml_close_element(writer, "D:lockscope") &&
+	wxml_open_element(bos, "D:locktype");
+	wxml_short_element(bos, "D:write");
+	wxml_close_element(bos, "D:locktype");
 
-		wxml_string_element(writer, "D:depth", "infinity") &&
+	wxml_open_element(bos, "D:lockscope");
+	wxml_short_element(bos, "D:exclusive");
+	wxml_close_element(bos, "D:lockscope");
 
-		locktoken_href(writer, token) &&
-		(data.owner_href.empty() ||
-		 owner_href(writer, data.owner_href.c_str())) &&
-		wxml_close_element(writer, "D:activelock") &&
-		wxml_close_element(writer, "D:lockdiscovery") &&
-		end_prop(writer);
+	wxml_string_element(bos, "D:depth", "infinity");
+
+	locktoken_href(bos, token);
+	if (!data.owner_href.empty())
+		owner_href(bos, data.owner_href.c_str());
+	wxml_close_element(bos, "D:activelock");
+	wxml_close_element(bos, "D:lockdiscovery");
+	end_prop(bos);
+
+	bos.Flush();
 }

src/propfind.cxx
@@ -10,6 +10,7 @@
 #include "error.hxx"
 #include "file.hxx"
 #include "Chrono.hxx"
+#include "was/WasOutputStream.hxx"
 #include "http/Date.hxx"
 #include "io/DirectoryReader.hxx"
 #include "util/Compiler.h"
@@ -48,31 +49,24 @@ try {
 	return {};
 }
 
-static bool
-propfind_file(Writer &writer, std::string &uri, std::string &path,
+static void
+propfind_file(BufferedOutputStream &o, std::string &uri, std::string &path,
 	      const struct stat &st,
 	      unsigned depth)
 {
-	if (!open_response_prop(writer, uri.c_str(), "HTTP/1.1 200 OK"))
-		return false;
+	open_response_prop(o, uri.c_str(), "HTTP/1.1 200 OK");
 
 	if (S_ISDIR(st.st_mode)) {
-		if (!resourcetype_collection(writer))
-			return false;
+		resourcetype_collection(o);
 	} else if (S_ISREG(st.st_mode)) {
-		if (!wxml_format_element(writer, "D:getcontentlength", "%llu",
-					 (unsigned long long)st.st_size))
-			return false;
+		wxml_fmt_element(o, "D:getcontentlength", "{}", st.st_size);
 	}
 
 	const auto mtime = ToSystemTime(st.st_mtim);
 
-	if (!wxml_string_element(writer, "D:getlastmodified",
-				 http_date_format(mtime)))
-		return false;
+	wxml_string_element(o, "D:getlastmodified", http_date_format(mtime));
 
-	if (!close_response_prop(writer))
-		return false;
+	close_response_prop(o);
 
 	if (depth > 0 && S_ISDIR(st.st_mode)) {
 		--depth;
@@ -95,24 +89,18 @@ propfind_file(Writer &writer, std::string &uri, std::string &path,
 			path.append(name);
 
 			struct stat st2;
-			bool success = true;
 			if (stat(path.c_str(), &st2) == 0) {
 				if (S_ISDIR(st2.st_mode))
 					/* directory URIs should end with a slash */
 					uri.push_back('/');
 
-				success = propfind_file(writer, uri, path, st2, depth);
+				propfind_file(o, uri, path, st2, depth);
 			}
 
 			uri.erase(uri_length);
 			path.erase(path_length);
-
-			if (!success)
-				return false;
 		}
 	}
-
-	return true;
 }
 
 void
@@ -135,14 +123,15 @@ handle_propfind(was_simple *was, const char *uri, const FileResource &resource)
 				   "text/xml; charset=\"utf-8\""))
 		return;
 
-	Writer writer(was);
-	if (!begin_multistatus(writer))
-		return;
+	WasOutputStream wos{was};
+	BufferedOutputStream bos{wos};
+
+	begin_multistatus(bos);
 
 	std::string uri2(uri);
 	std::string path2(resource.GetPath());
-	if (!propfind_file(writer, uri2, path2, resource.GetStat(), depth))
-		return;
+	propfind_file(bos, uri2, path2, resource.GetStat(), depth);
+	end_multistatus(bos);
 
-	end_multistatus(writer);
+	bos.Flush();
 }

src/proppatch.cxx
@@ -8,6 +8,7 @@
 #include "wxml.hxx"
 #include "expat.hxx"
 #include "error.hxx"
+#include "was/WasOutputStream.hxx"
 
 extern "C" {
 #include <was/simple.h>
@@ -75,33 +76,35 @@ char_data(void *userData, const XML_Char *s, int len)
 	}
 }
 
-static bool
-ns_short_element(Writer &w, const char *name)
+static void
+ns_short_element(BufferedOutputStream &o, const char *name)
 {
 	const char *pipe = strchr(name, '|');
 	if (pipe == nullptr)
-		return false;
+		// TODO what now? is this a bug or bad user input?
+		return;
 
 	const std::string ns(name, pipe);
 	name = pipe + 1;
 
-	return w.Write("<") && w.Write("X:") &&
-		w.Write(name) &&
-		wxml_attribute(w, "xmlns:X", ns.c_str()) &&
-		wxml_end_short_tag(w);
+	o.Write('<');
+	o.Write("X:");
+	o.Write(name);
+	wxml_attribute(o, "xmlns:X", ns.c_str());
+	wxml_end_short_tag(o);
 }
 
-static bool
-propstat(Writer &w, const char *name, const char *status)
+static void
+propstat(BufferedOutputStream &o, const char *name, std::string_view status)
 {
-	return wxml_open_element(w, "D:propstat") &&
-		wxml_open_element(w, "D:prop") &&
-		ns_short_element(w, name) &&
-		wxml_close_element(w, "D:prop") &&
-		wxml_open_element(w, "D:status") &&
-		w.Write(status) &&
-		wxml_close_element(w, "D:status") &&
-		wxml_close_element(w, "D:propstat");
+	wxml_open_element(o, "D:propstat");
+	wxml_open_element(o, "D:prop");
+	ns_short_element(o, name);
+	wxml_close_element(o, "D:prop");
+	wxml_open_element(o, "D:status");
+	o.Write(status);
+	wxml_close_element(o, "D:status");
+	wxml_close_element(o, "D:propstat");
 }
 
 [[gnu::pure]]
@@ -145,16 +148,20 @@ ProppatchMethod::SendResponse(was_simple *w, const char *uri)
 				   "text/xml; charset=\"utf-8\""))
 		return false;
 
-	Writer writer(w);
-	if (!begin_multistatus(writer) ||
-	    !wxml_open_element(writer, "D:response") ||
-	    !href(writer, uri))
-		return false;
+	WasOutputStream wos{w};
+	BufferedOutputStream bos{wos};
+
+	begin_multistatus(bos);
+	wxml_open_element(bos, "D:response");
+	href(bos, uri);
 
 	for (auto prop : data.props)
-		if (!propstat(writer, prop.name.c_str(),
-			      http_status_to_string(prop.status)))
-			return false;
+		propstat(bos, prop.name.c_str(),
+			 http_status_to_string(prop.status));
 
-	return wxml_close_element(writer, "D:response") && end_multistatus(writer);
+	wxml_close_element(bos, "D:response");
+	end_multistatus(bos);
+
+	bos.Flush();
+	return true;
 }

src/wxml.cxx
@@ -32,22 +32,21 @@ wxml_escape_char(char ch)
 	gcc_unreachable();
 }
 
-bool
-wxml_cdata(Writer &w, const char *data)
+void
+wxml_cdata(BufferedOutputStream &o, const char *data)
 {
 	while (true) {
 		const char *p = strpbrk(data, "<>&\"");
-		if (p == nullptr)
-			return w.Write(data);
+		if (p == nullptr) {
+			o.Write(data);
+			return;
+		}
 
 		if (p > data) {
-			if (!w.Write(data, p - data))
-				return false;
-
+			o.Write(std::string_view(data, p - data));
 			data = p;
 		}
 
-		if (!w.Write(wxml_escape_char(*data++)))
-			return false;
+		o.Write(wxml_escape_char(*data++));
 	}
 }

commit 012f11b2f0cbd72a52661a69f1f394bc086ce9be

wxml: fix duplicate "fmt" parameter
Regression by commit c5d54116444a88478cdaf96a1a00bccf9d88a061

src/wxml.hxx
@@ -71,7 +71,7 @@ wxml_fmt_element(BufferedOutputStream &o, std::string_view name,
 		 const S &fmt, Args&&... args)
 {
 	wxml_open_element(o, name);
-	o.Fmt(fmt, fmt, std::forward<Args>(args)...);
+	o.Fmt(fmt, std::forward<Args>(args)...);
 	wxml_close_element(o, name);
 }
 